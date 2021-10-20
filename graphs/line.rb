a3b158406dd9a8587417d46cf3bfbdc5f76c7f9b
phase <phase858@gmail.com>
2021-10-20 08:30:07 -0400

    more graphs

---
+++ graphs/line.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+
+require 'json'
+require 'gruff'
+
+raw_file = File.read(ARGV[1])
+data = JSON.parse raw_file
+
+g = Gruff::Line.new('800x600')
+g.theme = {
+  :colors => %w[orange purple green white red #cccccc],
+  :marker_color => '#8d8d8d',
+  :font_color => 'white',
+  :background_colors => %w[#1d1d1d #4a4a4a]
+}
+g.maximum_value = 120
+g.minimum_value = 0
+g.y_axis_increment = 15
+g.sort = false
+g.title = "Run ##{ARGV[0]}"
+# g.x_axis_label = 'Wagon'
+g.y_axis_label = 'Seconds'
+g.data('time', data)
+g.write(ARGV[2])
--- render.sh
+++ render.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-./graphs/render.rb $1 ./data/run$1.json ./graphs/run$1.png
+./graphs/bar.rb $1 ./data/run$1.json ./graphs/run$1-bar.png
+./graphs/line.rb $1 ./data/run$1.json ./graphs/run$1-line.png
